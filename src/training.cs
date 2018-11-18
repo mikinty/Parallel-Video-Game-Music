abe678e55e303c7b373e2632776121b3fcfd3eb0
Annie Xu <xu.annie.97@gmail.com>
2018-11-18 10:27:49 -0500

    Attempt to parse MIDI File

---
+++ src/training.cs
@@ -0,0 +1,37 @@
+
+using Melanchall.DryWetMidi.Smf;
+
+static int Main(string[] args){
+	 if (args.Length < 2)
+        {
+            System.Console.WriteLine("Please enter a MIDI File Path and a Text File Path.");
+            return 1;
+        }
+      ConvertMidiToText(args[0], args[1]);
+}
+
+public static int GetTranspose(MidiFile midiFile){
+	//Do something to get the Key
+	return 0;
+}
+
+public static void ConvertMidiToText(string midiFilePath, string textFilePath)
+{
+    var midiFile = MidiFile.Read(midiFilePath);
+    var channelSplit = MidiFile.SplitByChannel(midiFile);
+
+    foreach (var channelMidi in channelSplit)
+    {
+        var shift = GetTranspose(channelMidi);
+    	var avgNote = channelMidi.GetNotes().Average(n => n.NoteNumber);
+    	var count = channelMidi.GetNotes().Length;
+    
+        File.AppendAllText(textFilePath, $"{avgNote} {count}\n");
+
+		File.AppendAllText(textFilePath,
+                       	   channelMidi.GetNotes()
+                               .Select(n => $"{n.NoteNumber + shift} {n.Time} {n.Length}"));
+
+		File.AppendAllText(textFilePath, "\n");
+    }
+}
