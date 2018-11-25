e995c6dadf4b7564505d7acb15c209159ecebec4
mikinty <myou@andrew.cmu.edu>
2018-11-25 00:00:42 -0500

    title

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,5 +1,17 @@
-function Title(props) {
-  return (
-    <h1>{props.title}</h1>
-  );
+import React from 'react';
+
+export default class Title extends React.Component {
+  constructor (props) {
+    super();
+
+    this.state = {
+      title: props.title
+    };
+  }
+
+  render() {
+    return (
+      <h1>{this.state.title}</h1>
+    );
+  }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -7,33 +7,46 @@ import * as CONST from './CONST';
 
 // Components
 import RadioSelect from './components/RadioSelect';
+import Title from './components/Title';
+
+class App extends React.Component {
+  componentDidMount () {
+    document.title = CONST.TITLE;
+  }
+
+  render () {
+    return (
+      <div className='bg'>
+        <div className='title'>
+          <Title title={CONST.TITLE}/>
+        </div>
+
+        <div className='main'>
+          <RadioSelect />
+        </div>
+
+        <div className='buttons'>
+        <a href='#'class='playBut'>
+          
+          <svg version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px" y="0px" width="120px" height="120px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
+            space="preserve">
+          
+            <polygon class='triangle' id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "/>
+              
+            <circle class='circle' id="XMLID_17_" fill="none"  stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"/>
+          </svg>
+        </a>
+        </div>
+        
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <div className='bg'>
-    <div className='title'>
-      <h1>{CONST.TITLE}</h1>
-    </div>
-
-    <div className='main'>
-      <RadioSelect />
-    </div>
-
-    <div className='buttons'>
-    <a href='#'class='playBut'>
-      
-      <svg version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px" y="0px" width="120px" height="120px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
-        space="preserve">
-      
-        <polygon class='triangle' id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "/>
-          
-        <circle class='circle' id="XMLID_17_" fill="none"  stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"/>
-      </svg>
-    </a>
-    </div>
-    
-  </div>,
+  <App />,
   document.getElementById('app')
 );
 
