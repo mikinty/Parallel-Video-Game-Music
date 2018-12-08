33923692929566f115f3eba592fceff1814e3d6e
Annie Xu <xu.annie.97@gmail.com>
2018-12-08 17:44:09 -0500

    Begin music gen

---
+++ src/music gen/musicGen.h
@@ -0,0 +1,12 @@
+#define MAJORHIGH "majorHighMatrix.txt"
+#define MAJORLOW "majorLowMatrix.txt"
+#define MAJORCHORD "majorChordMatrix.txt"
+#define MINORHIGH "minorHighMatrix.txt"
+#define MINORLOW "minorLowMatrix.txt"
+#define MINORCHORD "minorChordMatrix.txt"
+
+float* highNotes;
+float* lowNotes;
+float* chords;
+
+int* parts; //part assignments for up to 
