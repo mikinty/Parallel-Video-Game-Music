3557f136804bb9cf57dbbc0cec86007fb6003cd9
Michael You <myou@andrew.cmu.edu>
2018-12-16 12:54:20 +0000

    need to drop the last row bc its nan

--- src/convert.py
+++ src/convert.py
@@ -17,7 +17,12 @@ for f in FILES:
 
   temp = pd.read_csv(DIRECTORY+f+'.txt', sep =' ', header=None)
 
+  # the last column is garbage
+  temp.drop(temp.columns[len(temp.columns)-1], axis=1, inplace=True)
+
   with open(f+'.pkl', 'wb') as fout:
     dump(temp.values, fout, protocol=4)
+
+  print('Shape:', temp.values.shape)
 
 print('Done converting all matrices to pickle')
